29c2a3624ce23235710f1c02caca73c92498e78e
João Simões <nogravity00@outlook.com>
2016-02-08 14:04:18 +0000

    Added IWorkArea

---
+++ SimplePersistence.UoW/src/SimplePersistence.UoW/IWorkArea.cs
@@ -0,0 +1,10 @@
+﻿namespace SimplePersistence.UoW
+{
+    /// <summary>
+    /// Represents a work area that can be used for aggregating
+    /// Unit of Work properties
+    /// </summary>
+    public interface IWorkArea
+    {
+    }
+}
