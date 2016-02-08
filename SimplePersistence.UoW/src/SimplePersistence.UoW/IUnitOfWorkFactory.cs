558a02dc72744f1f86b33dbd23d0fd892b553ebb
João Simões <nogravity00@outlook.com>
2016-02-08 16:49:29 +0000

    Added uow factory interface

---
+++ SimplePersistence.UoW/src/SimplePersistence.UoW/IUnitOfWorkFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace SimplePersistence.UoW
+{
+    using System;
+
+    /// <summary>
+    /// Interface to a factory of <see cref="IUnitOfWork"/> objects.
+    /// </summary>
+    public interface IUnitOfWorkFactory
+    {
+        /// <summary>
+        /// Gets a given <see cref="IUnitOfWork"/> of the specified type
+        /// </summary>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <returns>An <see cref="IUnitOfWork"/> instance</returns>
+        TUoW Get<TUoW>() where TUoW : IUnitOfWork;
+
+        /// <summary>
+        /// Gets a given <see cref="IUnitOfWork"/> of the specified type
+        /// </summary>
+        /// <param name="uowType">The <see cref="IUnitOfWork"/> type</param>
+        /// <returns>An <see cref="IUnitOfWork"/> instance</returns>
+        IUnitOfWork Get(Type uowType);
+
+        /// <summary>
+        /// Releases a given <see cref="IUnitOfWork"/> 
+        /// </summary>
+        /// <param name="unitOfWork">The instance to be released</param>
+        void Release(IUnitOfWork unitOfWork);
+    }
+}
