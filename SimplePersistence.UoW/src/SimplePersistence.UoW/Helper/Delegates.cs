d5b5dd166798c3bd5f813601f5df7049c3bb8512
João Simões <nogravity00@outlook.com>
2016-02-09 15:33:55 +0000

    Extracted delegates for .NET 2.0

---
+++ SimplePersistence.UoW/src/SimplePersistence.UoW/Helper/Delegates.cs
@@ -0,0 +1,10 @@
+﻿namespace SimplePersistence.UoW.Helper
+{
+#if NET20
+        public delegate void Action();
+        public delegate void Action<in T>(T t);
+
+        public delegate TResult Func<out TResult>();
+        public delegate TResult Func<in T, out TResult>(T t);
+#endif
+}
--- SimplePersistence.UoW/src/SimplePersistence.UoW/Helper/UnitOfWorkExtensions-Sync.cs
+++ SimplePersistence.UoW/src/SimplePersistence.UoW/Helper/UnitOfWorkExtensions-Sync.cs
@@ -151,14 +151,5 @@ namespace SimplePersistence.UoW.Helper
         }
 
         #endregion
-
-#if NET20
-        public delegate void Action();
-        public delegate void Action<in T>(T t);
-
-        public delegate TResult Func<out TResult>();
-        public delegate TResult Func<in T, out TResult>(T t);
-#endif
-
     }
 }
