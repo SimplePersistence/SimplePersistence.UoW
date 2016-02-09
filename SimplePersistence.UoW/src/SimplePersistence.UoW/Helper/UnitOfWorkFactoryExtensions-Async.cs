7c4f42aba1cf9cfa44804fdadd9a85dabef65133
João Simões <nogravity00@outlook.com>
2016-02-09 17:13:31 +0000

    Implemented UoW factory methods for async

--- SimplePersistence.UoW/src/SimplePersistence.UoW/Helper/UnitOfWorkFactoryExtensions-Async.cs
+++ SimplePersistence.UoW/src/SimplePersistence.UoW/Helper/UnitOfWorkFactoryExtensions-Async.cs
@@ -23,7 +23,386 @@
 #endregion
 namespace SimplePersistence.UoW.Helper
 {
+#if !(NET20 || NET35)
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Exceptions;
+
     public static partial class UnitOfWorkFactoryExtensions
     {
+        #region GetAndReleaseAsync
+
+        #region Task<T>
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function asynchronously, releases the UoW instance and returns the value
+        /// </summary>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <typeparam name="T">The result type</typeparam>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>A task that can be awaited for the result</returns>
+        /// <exception cref="ArgumentNullException"/>
+#if NET40 || PORTABLE40
+        public static Task<T> GetAndReleaseAsync<TFactory, TUoW, T>(
+            this TFactory factory, Func<TUoW, CancellationToken, Task<T>> toExecute,
+            CancellationToken ct = default(CancellationToken))
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (toExecute == null) throw new ArgumentNullException(nameof(toExecute));
+
+            var uow = factory.Get<TUoW>();
+
+            var tcs = new TaskCompletionSource<T>();
+            toExecute(uow, ct).ContinueWith(t01 =>
+            {
+                if (t01.IsFaulted)
+                {
+                    tcs.SetExceptionFromTask(t01);
+                }
+                else if (t01.IsCompleted)
+                {
+                    tcs.SetResult(t01.Result);
+                    factory.Release(uow);
+                }
+                else
+                {
+                    tcs.SetCanceled();
+                }
+            }, ct);
+
+            return tcs.Task;
+        }
+#else
+        public static async Task<T> GetAndReleaseAsync<TFactory, TUoW, T>(
+            this TFactory factory, Func<TUoW, CancellationToken, Task<T>> toExecute, CancellationToken ct = default (CancellationToken))
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (toExecute == null) throw new ArgumentNullException(nameof(toExecute));
+
+            var uow = factory.Get<TUoW>();
+            try
+            {
+                return await toExecute(uow, ct);
+            }
+            finally
+            {
+                factory.Release(uow);
+            }
+        }
+#endif
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function asynchronously, releases the UoW instance and returns the value
+        /// </summary>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <typeparam name="T">The result type</typeparam>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <returns>A task that can be awaited for the result</returns>
+        /// <exception cref="ArgumentNullException"/>
+#if NET40 || PORTABLE40
+        public static Task<T> GetAndReleaseAsync<TFactory, TUoW, T>(
+            this TFactory factory, Func<TUoW, Task<T>> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            return factory.GetAndReleaseAsync<TFactory, TUoW, T>((uow, ct) => toExecute(uow));
+        }
+#else
+        public static async Task<T> GetAndReleaseAsync<TFactory, TUoW, T>(
+            this TFactory factory, Func<TUoW, Task<T>> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            return await factory.GetAndReleaseAsync<TFactory, TUoW, T>(async (uow, ct) => await toExecute(uow));
+        }
+#endif
+
+        #endregion
+
+        #region Task
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function asynchronously, releases the UoW instance.
+        /// </summary>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>A task that can be awaited</returns>
+        /// <exception cref="ArgumentNullException"/>
+#if NET40 || PORTABLE40
+        public static Task GetAndReleaseAsync<TFactory, TUoW>(
+            this TFactory factory, Func<TUoW, CancellationToken, Task> toExecute,
+            CancellationToken ct = default(CancellationToken))
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (toExecute == null) throw new ArgumentNullException(nameof(toExecute));
+
+            var uow = factory.Get<TUoW>();
+
+            var tcs = new TaskCompletionSource<bool>();
+            toExecute(uow, ct).ContinueWith(t01 =>
+            {
+                if (t01.IsFaulted)
+                {
+                    tcs.SetExceptionFromTask(t01);
+                }
+                else if (t01.IsCompleted)
+                {
+                    tcs.SetResult(true);
+                    factory.Release(uow);
+                }
+                else
+                {
+                    tcs.SetCanceled();
+                }
+            }, ct);
+
+            return tcs.Task;
+        }
+#else
+        public static async Task GetAndReleaseAsync<TFactory, TUoW>(
+            this TFactory factory, Func<TUoW, CancellationToken, Task> toExecute,
+            CancellationToken ct = default(CancellationToken))
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (toExecute == null) throw new ArgumentNullException(nameof(toExecute));
+
+            var uow = factory.Get<TUoW>();
+            try
+            {
+                await toExecute(uow, ct);
+            }
+            finally
+            {
+                factory.Release(uow);
+            }
+        }
+#endif
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function asynchronously, releases the UoW instance.
+        /// </summary>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <returns>A task that can be awaited</returns>
+        /// <exception cref="ArgumentNullException"/>
+#if NET40 || PORTABLE40
+        public static Task GetAndReleaseAsync<TFactory, TUoW>(
+            this TFactory factory, Func<TUoW, Task> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            return factory.GetAndReleaseAsync<TFactory, TUoW>((uow, ct) => toExecute(uow));
+        }
+#else
+        public static async Task GetAndReleaseAsync<TFactory, TUoW>(
+            this TFactory factory, Func<TUoW, Task> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            await factory.GetAndReleaseAsync<TFactory, TUoW>(async (uow, ct) =>
+            {
+                await toExecute(uow);
+            });
+        }
+#endif
+
+        #endregion
+
+        #endregion
+
+        #region GetAndReleaseAfterExecuteAndCommitAsync
+
+        #region Task<T>
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function asynchronously, releases the UoW instance and returns the value. 
+        /// The function execution will be encapsulated inside a <see cref="IUnitOfWork.BeginAsync"/> 
+        /// and <see cref="IUnitOfWork.CommitAsync"/> scope.
+        /// </summary>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <typeparam name="T">The result type</typeparam>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>A task that can be awaited for the result</returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ConcurrencyException"/>
+        /// <exception cref="CommitException"/>
+#if NET40 || PORTABLE40
+        public static Task<T> GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW, T>(
+            this TFactory factory, Func<TUoW, CancellationToken, Task<T>> toExecute,
+            CancellationToken ct = default(CancellationToken))
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (toExecute == null) throw new ArgumentNullException(nameof(toExecute));
+
+            return factory.GetAndReleaseAsync<TFactory, TUoW, T>(
+                (uow01, ct01) => uow01.ExecuteAndCommitAsync(toExecute, ct01), ct);
+        }
+#else
+        public static async Task<T> GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW, T>(
+            this TFactory factory, Func<TUoW, CancellationToken, Task<T>> toExecute, CancellationToken ct = default(CancellationToken))
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (toExecute == null) throw new ArgumentNullException(nameof(toExecute));
+
+            return await factory.GetAndReleaseAsync<TFactory, TUoW, T>(async (uow01, ct01) =>
+            {
+                return await uow01.ExecuteAndCommitAsync(async (uow02, ct02) => await toExecute(uow02, ct02), ct01);
+            }, ct);
+        }
+#endif
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function asynchronously, releases the UoW instance and returns the value. 
+        /// The function execution will be encapsulated inside a <see cref="IUnitOfWork.BeginAsync"/> 
+        /// and <see cref="IUnitOfWork.CommitAsync"/> scope.
+        /// </summary>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <typeparam name="T">The result type</typeparam>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <returns>A task that can be awaited for the result</returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ConcurrencyException"/>
+        /// <exception cref="CommitException"/>
+#if NET40 || PORTABLE40
+        public static Task<T> GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW, T>(
+            this TFactory factory, Func<TUoW, Task<T>> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            return factory.GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW, T>((uow, ct) => toExecute(uow));
+        }
+
+#else
+        public static async Task<T> GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW, T>(
+            this TFactory factory, Func<TUoW, Task<T>> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            return await factory.GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW, T>(
+                async (uow, ct) => await toExecute(uow));
+        }
+#endif
+
+        #endregion
+
+        #region Task
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function asynchronously, releases the UoW instance. 
+        /// The function execution will be encapsulated inside a <see cref="IUnitOfWork.BeginAsync"/> 
+        /// and <see cref="IUnitOfWork.CommitAsync"/> scope.
+        /// </summary>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>A task that can be awaited</returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ConcurrencyException"/>
+        /// <exception cref="CommitException"/>
+#if NET40 || PORTABLE40
+        public static Task GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW>(
+            this TFactory factory, Func<TUoW, CancellationToken, Task> toExecute,
+            CancellationToken ct = default(CancellationToken))
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (toExecute == null) throw new ArgumentNullException(nameof(toExecute));
+
+            return factory.GetAndReleaseAsync<TFactory, TUoW>(
+                (uow01, ct01) => uow01.ExecuteAndCommitAsync(toExecute, ct01), ct);
+        }
+#else
+        public static async Task GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW>(
+            this TFactory factory, Func<TUoW, CancellationToken, Task> toExecute, CancellationToken ct = default(CancellationToken))
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (toExecute == null) throw new ArgumentNullException(nameof(toExecute));
+
+            await factory.GetAndReleaseAsync<TFactory, TUoW>(async (uow01, ct01) =>
+            {
+                await uow01.ExecuteAndCommitAsync(async (uow02, ct02) => await toExecute(uow02, ct02), ct01);
+            }, ct);
+        }
+#endif
+
+        /// <summary>
+        /// Gets an <see cref="IUnitOfWork"/> from the given <see cref="IUnitOfWorkFactory"/> and, after 
+        /// executing the function asynchronously, releases the UoW instance. 
+        /// The function execution will be encapsulated inside a <see cref="IUnitOfWork.BeginAsync"/> 
+        /// and <see cref="IUnitOfWork.CommitAsync"/> scope.
+        /// </summary>
+        /// <typeparam name="TFactory">The <see cref="IUnitOfWorkFactory"/> type</typeparam>
+        /// <typeparam name="TUoW">The <see cref="IUnitOfWork"/> type</typeparam>
+        /// <param name="factory">The factory to be used</param>
+        /// <param name="toExecute">The action to be executed</param>
+        /// <returns>A task that can be awaited</returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ConcurrencyException"/>
+        /// <exception cref="CommitException"/>
+#if NET40 || PORTABLE40
+        public static Task GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW>(
+            this TFactory factory, Func<TUoW, Task> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            return factory.GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW>((uow, ct) => toExecute(uow));
+        }
+
+#else
+        public static async Task GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW>(
+            this TFactory factory, Func<TUoW, Task> toExecute)
+            where TFactory : IUnitOfWorkFactory
+            where TUoW : IUnitOfWork
+        {
+            await factory.GetAndReleaseAfterExecuteAndCommitAsync<TFactory, TUoW>(
+                async (uow, ct) => await toExecute(uow));
+        }
+#endif
+
+        #endregion
+
+        #endregion
     }
+
+#endif
 }
