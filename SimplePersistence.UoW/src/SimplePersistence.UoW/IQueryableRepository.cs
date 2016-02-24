eb526604bfc2f0a7e0a7d500d7bed58cb1de3ad3
João Simões <nogravity00@outlook.com>
2016-02-24 14:27:04 +0000

    Added queryable repository interface

---
+++ SimplePersistence.UoW/src/SimplePersistence.UoW/IQueryableRepository.cs
@@ -0,0 +1,100 @@
+﻿#region License
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 SimplePersistence
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+namespace SimplePersistence.UoW
+{
+#if !NET20
+    using System.Linq;
+
+    /// <summary>
+    /// Repository interface that will be used to query and manipulate
+    /// persisted entities and can be used as an <see cref="IQueryable{T}"/> source
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId">The entity id type</typeparam>
+    public interface IQueryableRepository<TEntity, in TId>
+        : IRepository<TEntity, TId>, IExposeQueryable<TEntity, TId>
+        where TEntity : class
+    {
+
+    }
+
+    /// <summary>
+    /// Repository interface that will be used to query and manipulate
+    /// persisted entities and can be used as an <see cref="IQueryable{T}"/> source
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId01">The entity id first type</typeparam>
+    /// <typeparam name="TId02">The entity id second type</typeparam>
+    public interface IQueryableRepository<TEntity, in TId01, in TId02>
+        : IRepository<TEntity, TId01, TId02>, IExposeQueryable<TEntity, TId01, TId02>
+        where TEntity : class
+    {
+
+    }
+
+    /// <summary>
+    /// Repository interface that will be used to query and manipulate
+    /// persisted entities and can be used as an <see cref="IQueryable{T}"/> source
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId01">The entity id first type</typeparam>
+    /// <typeparam name="TId02">The entity id second type</typeparam>
+    /// <typeparam name="TId03">The entity id third type</typeparam>
+    public interface IQueryableRepository<TEntity, in TId01, in TId02, in TId03>
+        : IRepository<TEntity, TId01, TId02, TId03>, IExposeQueryable<TEntity, TId01, TId02, TId03>
+        where TEntity : class
+    {
+
+    }
+
+    /// <summary>
+    /// Repository interface that will be used to query and manipulate
+    /// persisted entities and can be used as an <see cref="IQueryable{T}"/> source
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId01">The entity id first type</typeparam>
+    /// <typeparam name="TId02">The entity id second type</typeparam>
+    /// <typeparam name="TId03">The entity id third type</typeparam>
+    /// <typeparam name="TId04">The entity id fourth type</typeparam>
+    public interface IQueryableRepository<TEntity, in TId01, in TId02, in TId03, in TId04>
+        : IRepository<TEntity, TId01, TId02, TId03, TId04>, IExposeQueryable<TEntity, TId01, TId02, TId03, TId04>
+        where TEntity : class
+    {
+
+    }
+
+    /// <summary>
+    /// Repository interface that will be used to query and manipulate
+    /// persisted entities and can be used as an <see cref="IQueryable{T}"/> source
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public interface IQueryableRepository<TEntity>
+        : IRepository<TEntity>, IExposeQueryable<TEntity>
+        where TEntity : class
+    {
+
+    }
+
+#endif
+}
