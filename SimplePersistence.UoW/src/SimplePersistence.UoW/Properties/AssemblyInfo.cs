72031884ed5f2ade557762cbd07c5b45f4c623fb
João Simões <nogravity00@outlook.com>
2016-02-28 00:51:17 +0000

    Unified Assembly Title for all platforms

--- SimplePersistence.UoW/src/SimplePersistence.UoW/Properties/AssemblyInfo.cs
+++ SimplePersistence.UoW/src/SimplePersistence.UoW/Properties/AssemblyInfo.cs
@@ -26,22 +26,7 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if PORTABLE40
-[assembly: AssemblyTitle("SimplePersistence.UoW Portable .NET 4.0")]
-#elif PORTABLE
-[assembly: AssemblyTitle("SimplePersistence.UoW Portable")]
-#elif DOTNET
-[assembly: AssemblyTitle("SimplePersistence.UoW .NET Platform")]
-#elif NET20
-[assembly: AssemblyTitle("SimplePersistence.UoW .NET 2.0")]
-#elif NET35
-[assembly: AssemblyTitle("SimplePersistence.UoW .NET 3.5")]
-#elif NET40
-[assembly: AssemblyTitle("SimplePersistence.UoW .NET 4.0")]
-#else
 [assembly: AssemblyTitle("SimplePersistence.UoW")]
-#endif
-
 [assembly: AssemblyDescription("SimplePersistence.UoW is a framework to help implement the Unit of Work pattern, by exposing interfaces for repositories that can be aggregated in work areas.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Net.JoaoSimoes")]
